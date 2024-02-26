c304abcf85b69b1847afc4c850f29d881ae952ac
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-26 09:34:20 +0000

    Added port='3307'

--- API/utils/database_connector.py
+++ API/utils/database_connector.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
-db = mysql.connector.connect(user="root", password="root", host="127.0.0.1", database="Memory_Game")
+db = mysql.connector.connect(user="root", password="root", host="127.0.0.1", port="3307", database="Memory_Game")
 cursor = db.cursor(dictionary=True)
