33ca84d50d3e7cf948f89b392395458af3b04ac4
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-26 10:30:44 +0000

    Created scores models

---
+++ API/models/scores.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class Score(BaseModel):
+    scoreID: int
+    score: int
+    difficulty: str
+    userID: int
+
+class CreateScore(BaseModel):
+    score: int
+    difficulty: str
