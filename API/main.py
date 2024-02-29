bdfb7da883a487fd595031ee3789d0d816629b09
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-29 15:21:45 +0000

    Added CORS configuration

--- API/main.py
+++ API/main.py
@@ -2,8 +2,11 @@
 # uvicorn main:app --reload
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.scores import app as scores_router
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(scores_router, prefix="/scores")
