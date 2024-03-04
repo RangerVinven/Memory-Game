d897893797198c54afa67d6e6409e693aebf7e16
Nuclearturtle12 <84713843+Nuclearturtle12@users.noreply.github.com>
2024-03-04 14:42:17 +0000

    create user
    create user end point made

--- API/routes/users.py
+++ API/routes/users.py
@@ -1,19 +1,45 @@
-from fastapi import APIRouter, Response, status, HTTPException
-
+from fastapi import APIRouter, Response, Request, status, HTTPException
+from models.users import CreateUser
 from utils.database_connector import db, cursor
 
 app = APIRouter()
 
-@app.get("")
+
+
+@app.get("/all")
+def listUsers():
+    cursor.execute("SELECT * FROM Users;")
+    return cursor.fetchall()
+
+
+#TODO create the session ID
+
 
 
 #TODO create the user
-@app.
+@app.post("/create")
+
+def createUser(user:CreateUser):
+    cursor.execute("INSERT INTO users (username, firstname, lastname, email, password) values (%s,%s,%s,%s,%s)", (user.username,user.firstname,user.lastname,user.email,user.password)
+)
+    db.commit()
+
+userID
+username varchar(20)
+firstname varchar(20)
+lastname varchar(25)
+email varchar(30)
+password varchar(64)
+
+session_token varchar(64)
+
 
 #TODO user login
-
+@app.post("/login")
 
 #TODO user deletion
-@app.delete("")
+@app.delete("/delete")
 
 #TODO edit user details
+@app.patch("/edit")
+
