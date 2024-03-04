047bd3789924840094a593333232faba3187ee9e
Nuclearturtle12 <84713843+Nuclearturtle12@users.noreply.github.com>
2024-03-04 15:36:14 +0000

    added logging in
    added logging in

--- Database/schema.sql
+++ Database/schema.sql
@@ -2,5 +2,5 @@ CREATE DATABASE Memory_Game;
 use Memory_Game;
 
 # Creates the tables
-CREATE TABLE Users(userID int auto_increment not null primary key, username varchar(20) not null, firstname varchar(20) not null, lastname varchar(25) not null, email varchar(30) not null, password varchar(64) not null, session_token varchar(64) not null);
+CREATE TABLE Users(userID int auto_increment not null primary key, username varchar(20) not null, firstname varchar(20) not null, lastname varchar(25) not null, email varchar(30) not null, password varchar(64) not null, session_token varchar(64));
 CREATE TABLE Scores(scoreID int auto_increment not null primary key, score int not null, difficulty varchar(6) not null, userID int);
